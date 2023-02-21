3d569e17bd5fca1cf7b5ccf2293454ca03aa68fe
Andre <andre.sammelselg@outlook.com>
2023-02-21 21:17:24 +0200

    Exercise 6 done

---
+++ pythonPractice/Warmup-1.py
@@ -0,0 +1,40 @@
+#1_sleep_in
+
+def sleep_in(weekday, vacation):
+  if not weekday or vacation:
+    return True
+  return False
+
+#2_monkey_trouble
+
+def monkey_trouble(a_smile, b_smile):
+  if a_smile == b_smile:
+    return True
+  return False
+
+#3_sum_double
+
+def sum_double(a, b):
+  sum = a + b
+  if a == b:
+    sum = sum * 2
+  return sum
+
+#4_diff21
+
+def diff21(n):
+  if n <= 21:
+    return 21 - n
+  else:
+    return (n - 21) * 2
+
+#5_parrot_trouble
+
+def parrot_trouble(talking, hour):
+  return (talking and (hour < 7 or hour > 20))
+
+#6_makes10
+
+def makes10(a, b):
+  return (a + b == 10 or a == 10 or b == 10)
+

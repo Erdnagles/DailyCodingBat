17cc57b57689b6d9f276fadbd2d3c8c17a6f5171
Andre <andre.sammelselg@outlook.com>
2023-03-04 22:39:30 +0200

    W2 Ex 5 done

--- pythonPractice/Warmup-2.py
+++ pythonPractice/Warmup-2.py
@@ -42,3 +42,15 @@ def string_splosion(str):
   for i in range(len(str)):
     result += str[:i+1]
   return result
+
+#5 last2
+
+def last2(str):
+  if len(str) < 2:
+    return 0
+  
+  count = 0
+  for i in range(len(str) - 2):
+    if (str[i:i+2] == str[-2:]):
+      count += 1
+  return count 

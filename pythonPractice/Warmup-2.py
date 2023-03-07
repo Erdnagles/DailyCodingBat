9af33020b1f00082a9573bd20c7ec17e378ea277
Andre <andre.sammelselg@outlook.com>
2023-03-07 22:08:46 +0200

    W2 Ex8 done

--- pythonPractice/Warmup-2.py
+++ pythonPractice/Warmup-2.py
@@ -74,3 +74,11 @@ def array_front9(nums):
     if nums[i] == 9:
       return True
   return False
+
+#8_array123
+
+def array123(nums):
+  for i in range(len(nums)-2):
+    if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
+      return True
+  return False

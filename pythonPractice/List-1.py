e9a6f1e2d020dfa0fb2249ae7709f409f80e39dd
Andre <andre.sammelselg@outlook.com>
2023-03-19 22:52:03 +0200

    List-1 Ex2 done.

--- pythonPractice/List-1.py
+++ pythonPractice/List-1.py
@@ -4,3 +4,10 @@ def first_last6(nums):
   if nums[0] == 6 or nums[-1] == 6:
     return True
   return False
+
+#2_same_first_last
+
+def same_first_last(nums):
+  if len(nums) >= 1 and nums[0] == nums[-1]:
+    return True
+  return False

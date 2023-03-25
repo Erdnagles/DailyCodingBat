5c0c812139b5c4c0d967d7afed68f145c53491f2
Andre <andre.sammelselg@outlook.com>
2023-03-25 16:55:32 +0200

    List-1 Ex 8 done.

--- pythonPractice/List-1.py
+++ pythonPractice/List-1.py
@@ -54,3 +54,9 @@ def reverse3(nums):
   for i in range(len(nums)-1, -1, -1):
     reversed.append(nums[i])
   return reversed
+
+#8_max_end3
+
+def max_end3(nums):
+  bigger = max(nums[0], nums[2])
+  return [bigger, bigger, bigger]

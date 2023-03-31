910e78160bda4632aa1c031af774f17ac4ec61c2
Andre <andre.sammelselg@outlook.com>
2023-03-31 19:46:47 +0300

    Logic-1 Ex 2 done.

--- pythonPractice/Logic-1.py
+++ pythonPractice/Logic-1.py
@@ -11,3 +11,14 @@ def cigar_party(cigars, is_weekend):
   if is_weekend == False:
     return cigars >= 40 and cigars <= 60
   return cigars >= 40
+
+#2_date_fashion
+
+def date_fashion(you, date):
+  if you <= 2 or date <= 2:
+    return 0
+
+  if you >= 8 or date >= 8:
+    return 2
+    
+  return 1

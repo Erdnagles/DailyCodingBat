1c95054b56fa5d616779bed8c577b82332dd988f
Andre <andre.sammelselg@outlook.com>
2023-04-09 21:50:57 +0300

    Logic-2 Ex 2 done.

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Python progress:
 2. Warmup-2 = 9/9 done
 3. String-1 = 11/11 done
 4. List-1 = 12/12 done
-5. Logic-1 = 1/9
-6. Logic-2 = 0/7
+5. Logic-1 = 9/9 done
+6. Logic-2 = 2/7
 7. String-2 = 0/6
 8. List-2 = 0/6
 
--- pythonPractice/Logic-2.py
+++ pythonPractice/Logic-2.py
@@ -9,3 +9,15 @@ def make_bricks(small, big, goal):
   goal = goal - (bricks_left * 5)
   
   return goal <= small
+
+#2_lone_sum
+
+def lone_sum(a, b, c):
+  sum = 0
+  if a != b and a != c:
+    sum += a
+  if b != a and b != c:
+    sum += b  
+  if c != a and c != b:
+    sum += c
+  return sum

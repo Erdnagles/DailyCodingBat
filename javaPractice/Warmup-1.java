a2dd6718fadff6777853d16ac8013273c05f2f92
Andre <andre.sammelselg@outlook.com>
2023-04-24 23:16:45 +0300

    Warmup-1 Ex 8 done.

--- javaPractice/Warmup-1.java
+++ javaPractice/Warmup-1.java
@@ -63,3 +63,11 @@ public boolean nearHundred(int n) {
     return true;
   return false;
 }
+
+// 8 posNeg
+
+public boolean posNeg(int a, int b, boolean negative) {
+  if (negative)
+    return (a < 0 && b < 0);
+  return (a < 0 && b >= 0 || a >= 0 && b < 0);
+}

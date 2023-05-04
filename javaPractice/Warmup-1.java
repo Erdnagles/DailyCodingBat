53b33331af8769623415f157753fa4af1515c57c
Andre <andre.sammelselg@outlook.com>
2023-05-04 22:31:31 +0300

    Warmup-1 Ex 15 done.

--- javaPractice/Warmup-1.java
+++ javaPractice/Warmup-1.java
@@ -122,3 +122,13 @@ public boolean or35(int n) {
     return false;
   }
 }
+
+// 15 front22
+
+public String front22(String str) {
+  if (str.length() <= 2) {
+    return str + str + str;
+  } else {
+    return str.substring(0, 2) + str + str.substring(0, 2);
+  }
+}

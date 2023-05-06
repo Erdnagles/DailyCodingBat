5ab5e226d1483cd0e5fccaf1b14a4c11ab05a194
Andre <andre.sammelselg@outlook.com>
2023-05-06 16:29:29 +0300

    Warmup-1 Ex 17 done.

--- javaPractice/Warmup-1.java
+++ javaPractice/Warmup-1.java
@@ -140,3 +140,9 @@ public boolean startHi(String str) {
     return true;
   return false;
 }
+
+// 17 icyHot
+
+public boolean icyHot(int temp1, int temp2) {
+  return temp1 < 0 && temp2 > 100 || temp1 > 100 && temp2 < 0;
+}

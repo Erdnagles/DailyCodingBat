ca54e9d3d1ff6ab99ea3a7e27147087779fc0016
Andre <andre.sammelselg@outlook.com>
2023-05-09 20:15:57 +0300

    Warmup-1 Ex 20 done.

--- javaPractice/Warmup-1.java
+++ javaPractice/Warmup-1.java
@@ -164,3 +164,12 @@ public boolean hasTeen(int a, int b, int c) {
   }
   return false;
 }
+
+// 20 loneTeen
+
+public boolean loneTeen(int a, int b) {
+  boolean aTeen = (a >= 13 && a <= 19);
+  boolean bTeen = (b >= 13 && b <= 19);
+  
+  return (aTeen && !bTeen) || (!aTeen && bTeen);
+}

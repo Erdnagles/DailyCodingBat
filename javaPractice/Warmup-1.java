3dfac05a4ccc95ddd40a87e811972f890808b188
Andre <andre.sammelselg@outlook.com>
2023-05-18 19:06:15 +0300

    Warmup-1 Ex 25 done.

--- javaPractice/Warmup-1.java
+++ javaPractice/Warmup-1.java
@@ -210,3 +210,12 @@ public int intMax(int a, int b, int c) {
     }
     return max;
 }
+
+// 25 close10
+
+public int close10(int a, int b) {
+  int diffA = Math.abs(a - 10);
+  int diffB = Math.abs(b - 10);
+  
+  return diffA < diffB ? a : diffB < diffA ? b : 0;
+}

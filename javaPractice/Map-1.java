85feba22d6b91fc80f121082036111bb13a0bc7e
Andre Sammelselg <andre.sammelselg@outlook.com>
2023-05-21 21:21:26 +0300

    Map-1 Ex 3 done.

--- javaPractice/Map-1.java
+++ javaPractice/Map-1.java
@@ -15,3 +15,12 @@ public Map<String, String> mapShare(Map<String, String> map) {
   }
   return map;
 }
+
+// 3
+
+public Map<String, String> mapAB(Map<String, String> map) {
+  if (map.containsKey("a") && map.containsKey("b")) {
+    map.put("ab", map.get("a") + map.get("b"));
+  }
+  return map;
+}
